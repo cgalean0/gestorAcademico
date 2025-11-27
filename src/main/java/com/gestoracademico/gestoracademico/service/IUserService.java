fc4d3cb0e62c96f8e5c39ef0fc9ea07605bbbae6
cgalean0 <cngaleano56@gmail.com>
2025-11-27 10:44:41 -0300

    Creacion de la abstraccion de los servicios del usuario

---
+++ src/main/java/com/gestoracademico/gestoracademico/service/IUserService.java
@@ -0,0 +1,26 @@
+package com.gestoracademico.gestoracademico.service;
+
+import com.gestoracademico.gestoracademico.dto.UserCreationDTO;
+import com.gestoracademico.gestoracademico.dto.UserDTO;
+import com.gestoracademico.gestoracademico.model.User;
+
+import java.util.List;
+
+public interface IUserService {
+    /**
+     * Create a new admin User in the database
+     * */
+    UserDTO createUser(UserCreationDTO user);
+    /**
+     * Update already exist User in the db
+     * */
+    UserDTO updateUser(Long id, User user);
+    /**
+     * Delete a User in the DB.
+     * */
+    void deleteUser(Long id);
+    /**
+     * Returns the List of Users in the DB
+     * */
+    List<UserDTO> listUsers();
+}
